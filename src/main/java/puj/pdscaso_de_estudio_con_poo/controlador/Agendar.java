c23dfb033a810923904a8c3360df85a9ed13ed88
Erick Garavito <sangaravi22@gmail.com>
2023-08-04 16:11:26 -0500

    Vista de agendar

---
+++ src/main/java/puj/pdscaso_de_estudio_con_poo/controlador/Agendar.java
@@ -0,0 +1,32 @@
+package puj.pdscaso_de_estudio_con_poo.controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class Agendar {
+
+    @FXML
+    private Button btnRegresar;
+
+    @FXML
+    private DatePicker dpFecha;
+
+    @FXML
+    private Spinner<?> spnHora;
+
+    @FXML
+    private TextField txtApellidos;
+
+    @FXML
+    private TextField txtCedula;
+
+    @FXML
+    private TextField txtEdad;
+
+    @FXML
+    private TextField txtNombres;
+
+}
