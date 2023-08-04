a53eafd093855aecf87b8c8dc019bbd67dfb7dd3
Erick Garavito <sangaravi22@gmail.com>
2023-08-04 15:48:39 -0500

    Vista de agendamiento de citas

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module puj.pdscaso_de_estudio_con_poo {
 
     opens puj.pdscaso_de_estudio_con_poo to javafx.fxml;
     exports puj.pdscaso_de_estudio_con_poo;
+    exports puj.pdscaso_de_estudio_con_poo.controlador;
+    opens puj.pdscaso_de_estudio_con_poo.controlador to javafx.fxml;
 }
---
+++ src/main/java/puj/pdscaso_de_estudio_con_poo/controlador/AgendamientoCitas.java
@@ -0,0 +1,17 @@
+package puj.pdscaso_de_estudio_con_poo.controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class AgendamientoCitas {
+
+    @FXML
+    private Button btnAgendar;
+
+    @FXML
+    void agendar(MouseEvent event) {
+
+    }
+
+}
