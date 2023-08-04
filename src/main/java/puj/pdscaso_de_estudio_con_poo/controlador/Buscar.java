ed2699dbd4a33de907dbcfe52b817eedc15c4d73
Erick Garavito <sangaravi22@gmail.com>
2023-08-04 16:31:43 -0500

    Vista de buscar

---
+++ src/main/java/puj/pdscaso_de_estudio_con_poo/controlador/Buscar.java
@@ -0,0 +1,33 @@
+package puj.pdscaso_de_estudio_con_poo.controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+
+public class Buscar {
+
+    @FXML
+    private Button btnBuscar;
+
+    @FXML
+    private Button btnRegresar;
+
+    @FXML
+    private ComboBox<?> cbFechasCitas;
+
+    @FXML
+    private TableView<?> tbCitas;
+
+    @FXML
+    void buscar(MouseEvent event) {
+
+    }
+
+    @FXML
+    void regresar(MouseEvent event) {
+
+    }
+
+}
